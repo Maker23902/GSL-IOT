52ab8e6ed6e292039c077f632a9eb635050d491a
Maker23902 <hkxcode@gmail.com>
2020-10-22 22:21:50 +0800

    增加脚本编辑器

---
+++ web/src/view/LoraWAN/LuaEditor.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <editor v-model="content"      
+        :content="content"
+        :lang="'lua'" 
+        theme="monokai" 
+        width="100%" 
+        height="500"
+        :options="{
+            enableBasicAutocompletion: true,
+            enableSnippets: true,
+            enableLiveAutocompletion: true,       
+            showPrintMargin:false,   //去除编辑器里的竖线
+        }"
+        @init="editorInit">
+        </editor>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'aceEditor',
+    components: {
+        editor: require('vue2-ace-editor'),
+    },
+    data() {
+        return {
+            content: ""
+        }
+    },   
+    methods: {
+        editorInit () {
+            require('brace/ext/language_tools') //language extension prerequsite...
+            require('brace/mode/lua') //language
+            require('brace/theme/monokai')
+            require('brace/snippets/lua') //snippet
+        }
+    }
+}
+</script>
